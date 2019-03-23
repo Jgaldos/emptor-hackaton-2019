eca41783e19d0c72844e19c3d06c0fd879dd8f29
Jose Galdos <jose@bitmaker.la>
2019-03-23 13:19:11 -0500

    Improve score output

--- app/services/score.py
+++ app/services/score.py
@@ -1,11 +1,4 @@
-# app.py
-from flask import Flask
 from flask_restful import Resource, Api, reqparse
-
-import json
-
-app = Flask(__name__)
-api = Api(app)
 
 parser = reqparse.RequestParser()
 parser.add_argument('profile')
@@ -24,18 +17,19 @@ class Score(Resource):
 
     def evaluate(self, profile_json):
 
-        # args = parser.parse_args()
-
-        # profile_json = args['profile']
-
-        # profile_data = json.loads(profile_json)
         profile_data = profile_json
 
-        id_data = id_score(profile_data['identification'])
-        name_data = name_score(profile_data['name'])
-        address_data = address_score(profile_data['address']['address'])
-        email_data = email_score(profile_data['email'])
-        phone_data = phone_score(profile_data['phone']['area_code'] + profile_data['phone']['number'])
+        profile_id = profile_data['identification']
+        profile_name = profile_data['name']
+        profile_address = profile_data['address']['address']
+        profile_phone = profile_data['phone']['area_code'] + profile_data['phone']['number']
+        profile_email = profile_data['email']
+
+        id_data = id_score(profile_id)
+        name_data = name_score(profile_name)
+        address_data = address_score(profile_address)
+        email_data = email_score(profile_email)
+        phone_data = phone_score(profile_phone)
 
         details = {
             'id': id_data['details'],
@@ -45,16 +39,33 @@ class Score(Resource):
             'phone': phone_data['details'],
         }
 
-        return {'id': id_data['score'],
+        total_score = (id_data['score'] + name_data['score'] + address_data['score'] + email_data['score'] + phone_data['score']) / 5
+
+        return {
+            "profile": {
+                "Id_document": profile_id,
+                "Name": profile_name,
+                "Address": profile_address,
+                "Phone": profile_phone,
+                "Email": profile_email,
+            },
+            "score": {
+                'id': id_data['score'],
                 'name': name_data['score'],
                 'address': address_data['score'],
                 'email': email_data['score'],
                 'phone': phone_data['score'],
-                'details': details}, 201
+                'details': details
+            },
+            # "total_score": sum(total_score_lst) / len(total_score_lst),
+            "total_score": total_score,
+
+        }, 201
+
 
 
 def address_score(address):
-    return {'score': 100,
+    return {'score': 0,
             'details': {
                 'gmaps': False
             }}
@@ -89,8 +100,3 @@ def name_score(name):
                 'cip': True,
             }}
 
-
-api.add_resource(Score, '/peru')
-
-if __name__ == '__main__':
-    app.run(debug=True)
